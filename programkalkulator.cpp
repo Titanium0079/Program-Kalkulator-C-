517a70f94070ccc45111decaca39abe9dd23851d
Titaniums0079 <titandeanova17042006@gmail.com>
2025-06-01 10:37:05 +0700

    Update programkalkulator.cpp
    program kalkulator di update dan debug sehingga program akan berjalan sesuai dengan fungsinya

--- programkalkulator.cpp
+++ programkalkulator.cpp
@@ -1,53 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main(){
-    float hasil;
-    int opsi;
-    float a, b;
-    cout << "====== PROGRAM KALKULATOR ======";
-    cout << "\n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n5. Keluar"<<endl;
-    cout << "Silahkan Pilih Salah Satu: ";
-    cin >>  opsi;
+    float angka1;
+    float angka2;
+    float hasil = 0;
+    int pilih;
 
-    if(opsi == 1){
-        cout <<" Input nilai a =  " ;
-        cin >> a;
-        cout <<" Input nilai b = " ;
-        cin >> b;
-        hasil = a + b;
-        cout << "Hasil Perhitungan adalah = " << hasil ;
+    cout << "===== KALKULATOR SEDERHANA =====" << endl;
+    cout << "Masukkan angka pertama: ";
+    cin >> angka1;
+    cout << "Masukkan angka kedua: ";
+    cin >> angka2;
+    cout << endl;
 
+    cout << "===== MENU KALKULATOR SEDERHANA =====" << endl;
+    cout << "HANYA 2 BILANGAN" << endl;
+    cout << "1. Program Penjumlahan\n";
+    cout << "2. Program Pengurangan\n";
+    cout << "3. Program Perkalian\n";
+    cout << "4. Program Pembagian\n";
+    cout << "5. Keluar dari program\n";
+
+    cout << "Pilihan Menu: ";
+    cin >> pilih;
+    if (pilih == 1)
+    {   hasil = angka1 + angka2;
+        cout << angka1 << " + " << angka2 << " = " << hasil;
+    } else if (pilih == 2){
+        hasil = angka1 - angka2;
+        cout << angka1 << " - " << angka2 << " = " << hasil;
+    }else if (pilih == 3){
+        hasil = angka1 * angka2;
+        cout << angka1 << " * " << angka2 << " = " << hasil;
+    }else if (pilih == 4){
+        hasil = angka1 / angka2;
+        cout << angka1 << " / " << angka2 << " = " << hasil;
+    }else if (pilih == 5){
+        cout << "Terimakasih telah menggunakan program kalkulator!";
+    }else{
+        cout << "Program error!" << endl;
     }
-     else if(opsi == 2){
-            cout <<" Input nilai a = " ;
-            cin >> a;
-            cout <<" Input nilai b = ";
-            cin >> b;
-            hasil = a - b;
-            cout << "Hasil Pengurangan adalah = " << hasil ;
-        }
-         else if(opsi == 3){
-                cout <<" Input nilai a = ";
-                cin >> a;
-                cout <<" Input nilai b = ";
-                cin >> b;
-                hasil = a * b;
-                cout << " Hasil Perkalian adalah = " << hasil;
-         }
-            else if(opsi == 4){
-                cout <<" Input nilai a = ";
-                cin >> a;
-                cout <<" Input nilai b = ";
-                cin >> b;
-                hasil = a / b;
-                cout << "Hasil Pembagian adalah = " << hasil;
-            }
-                    else if(opsi == 5){
-                        cout << " Terimakasih telah menggunakan Program Kalkulator!";
-                    }
-        else {
-            cout << " Opsi Tidak Valid!! " << endl;
-        }
-    
-
 }
